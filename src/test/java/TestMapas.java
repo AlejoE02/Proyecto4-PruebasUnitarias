46abbabfdc12fd42ee8f6095d6203314c0d6faf5
Alejandro Espitia <alejoespi1007@gmail.com>
2019-03-17 20:12:54 -0500

    finall

--- src/test/java/TestMapas.java
+++ src/test/java/TestMapas.java
@@ -7,9 +7,7 @@
 import com.mycompany.mapascoe.Persona;
 import com.mycompany.mapascoe.Principal;
 import java.util.HashMap;
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
